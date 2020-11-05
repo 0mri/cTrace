c94b7515769d6813373905e2ea21a180a876774f
Omri <omri@efra.im>
2020-11-05 17:26:44 +0200

    add json import

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 // #include "Session.h"
+#include <fstream>
 #include <json.hpp>
 using json = nlohmann::json;
 
@@ -7,14 +8,15 @@ using namespace std;
 
 int main(int argc, char **argv)
 {
-    if (argc != 2)
-    {
-        cout << "usage cTrace <config_path>" << endl;
-        return 0;
-    }
-    // std::ifstream i("config.json");
-    // json j;
-    // i >> j;
+    // if (argc != 2)
+    // {
+    //     cout << "usage cTrace <config_path>" << endl;
+    //     return 0;
+    // }
+    std::ifstream i("config1.json");
+    json j;
+    i >> j;
+    cout << j["students"]["omer"] << endl;
     // Session sess(argv[1]);
     return 0;
 };
